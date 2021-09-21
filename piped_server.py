19a365aed6fad2e5b14db341aa59fd84ad607aec
anuragpeshne <anurag.peshne@gmail.com>
2021-09-20 23:35:05 -0700

    piped server init

---
+++ piped_server.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import os
+import queue
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+q = None
+
+@app.get("/")
+def handle_info():
+    """
+    This function is called when the snake is registered.
+    See https://docs.battlesnake.com/guides/getting-started#step-4-register-your-battlesnake
+    """
+    print("INFO")
+    default_info = {
+        "apiversion": "1",
+        "author": "Anurag Peshne",
+        "color": "#fbdd74",
+        "head": "beluga",
+        "tail": "curled"
+    }
+
+    return __get_from_q(default_info)
+
+@app.post("/start")
+def handle_start():
+    """
+    This function is called everytime the snake is entered into a game.
+    request.json contains information about the game that's about to be played.
+    """
+    data = request.get_json()
+
+    print(data)
+    print(f"{data['game']['id']} START")
+    return "ok"
+
+@app.post("/move")
+def handle_move():
+    """
+    This function is called on every turn of a game.
+    Return a move from ["up", "down", "left" or "right"].
+    """
+    data = request.get_json()
+
+    print(data)
+    possible_moves = ["up", "down", "left" or "right"]
+    move = random.choice(possible_moves)
+
+    return {"move": move}
+
+
+@app.post("/end")
+def end():
+    """
+    This function is called when a game your snake was in ends.
+    It's purely for informational purposes, you don't have to make any decisions here.
+    """
+    data = request.get_json()
+
+    print(f"{data['game']['id']} END")
+    return "ok"
+
+def __get_from_q(default_value):
+    try:
+        return q.get(True, 500 / 1000)
+    except queue.Empty:
+        return default_value
+
+if __name__ == "__main__":
+    print("Starting Battlesnake Server...")
+    q = queue.Queue()
+    port = int(os.environ.get("PORT", "8080"))
+    app.run(host="0.0.0.0", port=port, debug=True)
