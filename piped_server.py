ae07362007f1e21a21da479780fe421c48870bd8
anuragpeshne <anurag.peshne@gmail.com>
2021-09-25 07:30:27 -0700

    connected wrapper and piped server, basic piping working

--- piped_server.py
+++ piped_server.py
@@ -2,6 +2,8 @@
 
 import os
 import queue
+import random
+import threading
 
 from flask import Flask
 from flask import request
@@ -16,7 +18,7 @@ def handle_info():
     This function is called when the snake is registered.
     See https://docs.battlesnake.com/guides/getting-started#step-4-register-your-battlesnake
     """
-    print("INFO")
+    #print("INFO")
     default_info = {
         "apiversion": "1",
         "author": "Anurag Peshne",
@@ -25,7 +27,7 @@ def handle_info():
         "tail": "curled"
     }
 
-    return __get_from_q(default_info)
+    return default_info
 
 @app.post("/start")
 def handle_start():
@@ -33,11 +35,13 @@ def handle_start():
     This function is called everytime the snake is entered into a game.
     request.json contains information about the game that's about to be played.
     """
+    global out_q
+    
+    #print("server start")
     data = request.get_json()
-    out_q.put(data)
+    out_q.put(('start', data))
 
-    print(data)
-    print(f"{data['game']['id']} START")
+    #print(f"{data['game']['id']} START")
     return "ok"
 
 @app.post("/move")
@@ -46,10 +50,13 @@ def handle_move():
     This function is called on every turn of a game.
     Return a move from ["up", "down", "left" or "right"].
     """
+    global in_q
+    global out_q
+    
     data = request.get_json()
-    out_q.put(data)
+    out_q.put(("move", data))
 
-    print(data)
+    #print(data)
     possible_moves = ["up", "down", "left" or "right"]
     random_move = random.choice(possible_moves)
 
@@ -59,15 +66,17 @@ def handle_move():
 
 
 @app.post("/end")
-def end():
+def handle_end():
     """
     This function is called when a game your snake was in ends.
     It's purely for informational purposes, you don't have to make any decisions here.
     """
-    data = request.get_json()
-    out_q.put(data)
+    global out_q
 
-    print(f"{data['game']['id']} END")
+    data = request.get_json()
+    out_q.put(('end', data))
+
+    #print(f"{data['game']['id']} END")
     return "ok"
 
 def __get_from_q(q, default_value):
@@ -77,8 +86,20 @@ def __get_from_q(q, default_value):
         return default_value
 
 if __name__ == "__main__":
-    print("Starting Battlesnake Server...")
+    #print("Starting Battlesnake Server...")
     in_q = queue.Queue()
     out_q = queue.Queue()
     port = int(os.environ.get("PORT", "8080"))
     app.run(host="0.0.0.0", port=port, debug=True)
+
+def start_server(in_q_param, out_q_param, port_param="8080"):
+    reset_q(in_q_param, out_q_param)
+    port = int(os.environ.get("PORT", port_param))
+    threading.Thread(target=lambda: app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)).start()
+
+def reset_q(in_q_param, out_q_param):
+    global in_q
+    global out_q
+    
+    in_q = in_q_param
+    out_q = out_q_param
--- wrapper.py
+++ wrapper.py
@@ -1,19 +1,93 @@
 #!/usr/bin/env python
 
+import queue
+import time
 
-# TODO: implement this
-def reset():
-    pass
+from os.path import expanduser
+from subprocess import Popen
 
-# TODO: implement this
+import piped_server
+
+BATTLESNAKE_EXE = expanduser("~") + "/go/bin/battlesnake"
+WORLD_W = 11
+WORLD_H = 11
+SNAKE_NAME = "Test"
+SNAKE_URL = "http://127.0.0.1:8080"
+MOVE_TIMEOUT = 1000
+
+server_pipe_in = queue.Queue()
+server_pipe_out = queue.Queue()
+battlesnake_process = None
+piped_server_started = False
+
+def reset(train_mode=False):
+    global server_pipe_in
+    global server_pipe_out
+    global battlesnake_process
+    
+    server_pipe_in = queue.Queue()
+    server_pipe_out = queue.Queue()
+    piped_server.reset_q(server_pipe_in, server_pipe_out)
+
+    return start(train_mode)
+
+def start(train_mode):
+    global battlesnake_process
+    global piped_server_started
+
+    delay = 100
+    if train_mode:
+        delay = 1000
+
+    if not piped_server_started:
+        piped_server.start_server(server_pipe_in, server_pipe_out)
+        piped_server_started = True
+        
+        # TODO poll /
+        time.sleep(2)
+    
+    battlesnake_process = Popen([
+        BATTLESNAKE_EXE,
+        "play",
+        "--delay", str(delay),
+        "--gametype", "solo", # "standard",
+        "--height", str(WORLD_H),
+        "--width", str(WORLD_W),
+        "--timeout", str(MOVE_TIMEOUT),
+        "--name", SNAKE_NAME,
+        "--url", SNAKE_URL,
+    ])
+
+    _ = server_pipe_out.get(True, 500 / 1000)
+    endpoint, start_state_out = server_pipe_out.get(True, 500 / 1000)
+    next_state, reward, done = parse_server_out(endpoint, start_state_out)
+    return (next_state, reward, done)
+
 def step(action):
-    # TODO: return (next_state, reward, done)
-    pass
+    server_pipe_in.put(action)
+    endpoint, output_data = server_pipe_out.get(True, 500 / 1000)
+    next_state, reward, done = parse_server_out(endpoint, output_data)
+    return (next_state, reward, done)
 
-# TODO: implement this
 def close():
-    # TODO: close all open handlers etc
-    pass
+    # TODO: close pipe server
 
+    exit(0)
+    
 if __name__ == "__main__":
     pass
+
+def parse_server_out(endpoint, out_data):
+    if endpoint == "start":
+        done = False
+        reward = 0
+        next_state = None
+    elif endpoint == "move":
+        done = False
+        reward = 0
+        next_state = out_data
+    else:
+        done = True
+        reward = 0
+        next_state = out_data
+    return (next_state, reward, done)
